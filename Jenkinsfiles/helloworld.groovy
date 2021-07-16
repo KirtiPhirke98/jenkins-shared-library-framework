cca4eec757c6e0a7c2d4dc52d96183df9a473c92
Kirti Phirke <54802757+KirtiPhirke98@users.noreply.github.com>
2021-07-16 16:58:18 +0530

    Update helloworld.groovy

--- Jenkinsfiles/helloworld.groovy
+++ Jenkinsfiles/helloworld.groovy
@@ -5,7 +5,7 @@ pipeline {
     stages {
         stage('build') {
             steps {
-                HelloWorld 'testing'
+                echo 'HelloWorld testing'
                 echo "Into Umbrella360 Build"
         checkout([$class: 'GitSCM', branches: [[name: 'feature/docker-build']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'umbrella360']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'Bitbucket_Sevice_Account', url: 'https://bitbucket.org/tarionwarranty/umbrella360.git']]])
         treeMapData = git(branch: 'feature/docker-build', credentialsId: 'Bitbucket_Sevice_Account', url: 'https://bitbucket.org/tarionwarranty/umbrella360.git')
